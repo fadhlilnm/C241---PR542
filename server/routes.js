c81ec897f2455da71a0e8d3af5809562af0d1d31
fadhlilnm <122024435+fadhlilnm@users.noreply.github.com>
2024-06-21 02:38:26 +0700

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,39 +1,57 @@
+require('dotenv').config();
 const express = require('express');
 const router = express.Router();
-const csv = require('csvtojson');
 const path = require('path');
+const csv = require('csvtojson');
 
-router.get('/test', (req, res) => {
-  const csvFilePath = path.join(__dirname, 'recipe', 'data-timur.csv');
-  console.log(`Testing file path: ${csvFilePath}`);
-  res.send({ filePath: csvFilePath });
-});
+const getCsvFilePath = (area) => {
+  const areaMap = {
+    'utara': 'new_data_north_jakarta_city.csv',
+    'timur': 'new_data_east_jakarta_city.csv',
+    'barat': 'new_data_west_jakarta_city.csv',
+    'selatan': 'new_data_south_jakarta_city.csv',
+    'pusat': 'new_data_central_jakarta_city.csv'
+  };
 
+  return `${process.env.PATH_TO_CSV_FILES}/${areaMap[area]}`
+};
 
-// get router area timur
-router.get('/timur', (req, res) => {
-  const csvFilePath = path.join(__dirname, 'data-timur.csv');
+router.get('/:area', async (req, res) => {
+  const { area } = req.params;
+  const csvFilePath = getCsvFilePath(area);
 
-  csv().fromFile(csvFilePath)
-    .then((jsonObj) => {
-      const csvFilePath = jsonObj.map((data, i) => {
-        return {
-          id: i,
-          name: data.name,
-          place_type: data.place_type,
-          address: data.address,
-          rating: data.rating,
-          latitude: data.latitude,
-          longitude: data.longitude,
-          hours: data.hours
-        };
-      });
-      res.send(csvFilePath);
-    })
-    .catch((error) => {
-      res.status(500).send({ error: 'Failed to load and process CSV file' });
-    });
-    console.log(`Testing file path: ${csvFilePath}`);
+  if (!csvFilePath) {
+    return res.status(400).send({ error: 'Invalid area specified' });
+  }
+
+  console.log(`Loading data for area: ${area} from file: ${csvFilePath}`);
+  
+  try {
+    const jsonObj = await csv().fromFile(csvFilePath);
+
+    const formatDataWarkop = jsonObj.map((data, i) => ({
+      id: i,
+      name: data.name,
+      place_type: data.place_type,
+      address: data.address,
+      rating: data.rating,
+      rating_count:data.rating_count,
+      hours: data.new_column_1,
+      parking:data.new_column_2,
+      ac:data.new_column_3,
+      wifi:data.wifnew_column_4,
+      toilet:data.new_column_5
+
+    }));
+
+    console.log(jsonObj)
+    const responseData = formatDataWarkop;
+
+    res.json(responseData);
+  } catch (error) {
+    console.error(`Error processing CSV data file for area ${area}:`, error);
+    res.status(500).send({ error: 'Failed to load CSV file' });
+  }
 });
 
 module.exports = router;
