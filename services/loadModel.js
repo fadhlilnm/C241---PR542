b01321994c9bb475b4f3f6dfc34348e30a32def8
fadhlilnm <122024435+fadhlilnm@users.noreply.github.com>
2024-06-21 02:39:39 +0700

    Update loadModel.js

--- services/loadModel.js
+++ services/loadModel.js
@@ -1,7 +1,22 @@
 // const tf = require("@tensorflow/tfjs-node");
+// require("dotenv").config();
 
-// async function loadModel() {
-//   return tf.loadGraphModel(process.env.MODEL_URL);
-// }
+// const loadModel = async () => {
+//   const modelUrl = process.env.MODEL_URL;
 
-// module.exports = loadModel
+//   if (!modelUrl) {
+//     throw new Error('MODEL_URL environment variable must be set.');
+//   }
+
+//   const model = await tf.loadLayersModel(modelUrl);
+
+//   model.predict = async (data) => {
+//     const inputTensor = tf.tensor(data);
+//     const predictions = model.predict(inputTensor).dataSync();
+//     return Array.from(predictions);
+//   };
+
+//   return model;
+// };
+
+// module.exports = loadModel;
