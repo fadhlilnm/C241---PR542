bebf65f08a2d2fd196bc524f76eab7d5fa56f757
fadhlilnm <122024435+fadhlilnm@users.noreply.github.com>
2024-06-21 02:40:40 +0700

    Update storeData.js

--- services/storeData.js
+++ services/storeData.js
@@ -2,7 +2,7 @@ const { Firestore } = require("@google-cloud/firestore");
 const db = new Firestore ();
 
 const storeData = async ( data ) => {
-  const collection = db.collection ('warkop');
+  const collection = db.collection ('predictions');
   const doc = collection.doc (data.id);
 
   await doc.set (data);
